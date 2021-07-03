1dc6cc48051d7e3eb2a8f665029a4e379afc322c
a.avramenko <a.avramenko@cardsmobile.ru>
2021-07-03 16:57:50 +0300

    Initial Commit

---
+++ GitDemo/GitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitDemoApp.swift
+//  GitDemo
+//
+//  Created by Artem Avramenko on 03.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct GitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
